31846354224c47266efcd317c4977d0bee888913
Mathu-Mitha-P <pjmathumithait@gmail.com>
2024-12-26 11:00:46 +0530

    Add files via upload

---
+++ JAVASCRIPT/contactdata.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 26, 2024 at 05:22 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `contactdata`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contactdata`
+--
+
+CREATE TABLE `contactdata` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(55) NOT NULL,
+  `lastname` varchar(55) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `email` varchar(55) NOT NULL,
+  `message` text NOT NULL,
+  `attachement` varchar(100) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `contactdata`
+--
+
+INSERT INTO `contactdata` (`id`, `firstname`, `lastname`, `phone`, `email`, `message`, `attachement`) VALUES
+(4, 'Arun', 'Raj', '9807654321', 'arun@arun.com', 'i need water in anna nagar', NULL),
+(5, 'arun', 'Raj', '9807654321', 'arun@arun.com', 'i need water in anna nagar', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contactdata`
+--
+ALTER TABLE `contactdata`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contactdata`
+--
+ALTER TABLE `contactdata`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ JAVASCRIPT/index.sql
@@ -0,0 +1,7 @@
+CREATE TABLE ContactForm 
+(
+  name varchar(30) NOT NULL,
+  email varchar(30) NOT NULL,
+  phone varchar(15) NOT NULL,
+  message text NOT NULL
+);
